051db4780d6b6b061a60dea77fa1d9615fe6c2b3
Dchauha5 <Dchauha5@uncc.edu>
2020-12-17 02:49:03 -0500

    new files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ErrorComponent } from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'signup',
+  component: SignupComponent
+},
 
+{
+  path: '**',
+  component: ErrorComponent
+}
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
